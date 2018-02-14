db17adfe311182986d875bff58d639612b877c51
Scott <syhlion@gmail.com>
2018-02-14 10:51:12 +0800

    add test AddBasicAuth

--- client_test.go
+++ client_test.go
@@ -1,11 +1,13 @@
 package greq
 
 import (
+	"bytes"
+	"encoding/base64"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 	"time"
 
@@ -96,19 +98,55 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	return
 
 }
-func init() {
-	worker = requestwork.New(10)
-	http.HandleFunc("/get", getHandler)
-	http.HandleFunc("/post", postHandler)
-	http.HandleFunc("/put", putHandler)
-	http.HandleFunc("/delete", deleteHandler)
-	go func() {
-		log.Fatal(http.ListenAndServe(":8080", nil))
-	}()
 
+func basicAuthHandler(w http.ResponseWriter, r *http.Request) {
+	basicAuthPrefix := "Basic "
+	auth := r.Header.Get("Authorization")
+	if strings.HasPrefix(auth, basicAuthPrefix) {
+		payload, err := base64.StdEncoding.DecodeString(auth[len(basicAuthPrefix):])
+		if err == nil {
+			pair := bytes.SplitN(payload, []byte(":"), 2)
+			if len(pair) == 2 && bytes.Equal(pair[0], []byte("scott")) && bytes.Equal(pair[1], []byte("fine")) {
+				w.Write([]byte("success"))
+				return
+			}
+		}
+	}
+	w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+	w.WriteHeader(http.StatusUnauthorized)
+	return
+}
+
+func TestAddBasicAuth(t *testing.T) {
+	worker = requestwork.New(10)
+	ts := httptest.NewServer(http.HandlerFunc(basicAuthHandler))
+	defer ts.Close()
+	client := New(worker, 15*time.Second)
+	client.SetBasicAuth("scott", "fine")
+	data, s, err := client.Get(ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s != http.StatusOK {
+		t.Fatalf("status fatal:%d ,body:%s", s, string(data))
+	}
+	if string(data) != "success" {
+		t.Fatal("body fatal :", string(data))
+	}
+
+	//test err response
+	client.SetBasicAuth("scott", "nofine")
+	data, s, err = client.Get(ts.URL, nil)
+	if s == http.StatusOK {
+		t.Fatalf("status fatal:%d ,body:%s", s, string(data))
+	}
+	if string(data) == "success" {
+		t.Fatal("body fatal :", string(data))
+	}
 }
 
 func TestGet(t *testing.T) {
+	worker = requestwork.New(10)
 	ts := httptest.NewServer(http.HandlerFunc(getHandler))
 	defer ts.Close()
 
@@ -128,6 +166,7 @@ func TestGet(t *testing.T) {
 
 }
 func TestPost(t *testing.T) {
+	worker = requestwork.New(10)
 	ts := httptest.NewServer(http.HandlerFunc(postHandler))
 	defer ts.Close()
 	client := New(worker, 15*time.Second)
@@ -145,6 +184,7 @@ func TestPost(t *testing.T) {
 	}
 }
 func TestPut(t *testing.T) {
+	worker = requestwork.New(10)
 	ts := httptest.NewServer(http.HandlerFunc(putHandler))
 	defer ts.Close()
 	client := New(worker, 15*time.Second)
@@ -162,6 +202,7 @@ func TestPut(t *testing.T) {
 	}
 }
 func TestDelete(t *testing.T) {
+	worker = requestwork.New(10)
 	ts := httptest.NewServer(http.HandlerFunc(deleteHandler))
 	defer ts.Close()
 	client := New(worker, 15*time.Second)
