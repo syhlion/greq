d6b22acbb5f31c6bcd8410820dc57b6adca7f955
Scott <syhlion@gmail.com>
2018-08-10 12:09:53 +0800

    fix post raw header

--- client.go
+++ client.go
@@ -282,11 +282,6 @@ func (c *Client) resolveRawRequest(req *http.Request, bb io.Reader, e error) (da
 	defer cancel()
 	c.resolveHeaders(req)
 
-	switch req.Method {
-	case "PUT", "POST", "DELETE":
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-	}
-
 	err = c.worker.Execute(ctx, req, func(resp *http.Response, err error) (er error) {
 		if err != nil {
 			return err
