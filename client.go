a3b1501647eedd9150c410894362c06135571db1
Scott <syhlion@gmail.com>
2019-02-18 16:36:03 +0800

    fix GetOnceHeader

--- client.go
+++ client.go
@@ -144,7 +144,7 @@ func (c *Client) Get(url string, params url.Values) (data []byte, httpstatus int
 
 }
 func (c *Client) GetWithOnceHeader(url string, params url.Values, headers map[string]string) (data []byte, httpstatus int, err error) {
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(params.Encode()))
+	req, err := http.NewRequest(http.MethodGet, url, strings.NewReader(params.Encode()))
 	if err != nil {
 		return
 	}
